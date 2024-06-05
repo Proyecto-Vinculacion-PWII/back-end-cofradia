98fb9f73fd40fa9f1b4ddb813669a037539f85ea
Ddgodoy16 <elvinnu_es25@outlook.es>
2024-06-05 16:30:04 -0500

    login

--- paneladmind/src/Componentes/Login.jsx
+++ paneladmind/src/Componentes/Login.jsx
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
 export const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alert, setAlert] = useState('');
   const navigate = useNavigate();
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate('/dashboard');
+    if (email === 'cofradiaong@gmail.com' && password === 'cofradia') {
+      navigate('/dashboard');
+    } else {
+      setAlert('Credenciales incorrectas. Por favor, inténtelo de nuevo.');
+    }
   };
 
   return (
     <div>
       <div className="login container">
         <h1 className="text-center mb-5">Inicio de sesión</h1>
+    
         <form className="d-flex flex-column align-items-center" onSubmit={handleSubmit}>
           <div className="form-group w-50 mb-3">
-            <label className="login label">Usuario</label>
-            <input type="email" className="form-control" name="correo"  />
+            <label className="login label">Correo</label>
+            <input 
+              type="email" 
+              className="form-control" 
+              name="correo" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} 
+            />
           </div>
           <div className="form-group w-50 mb-3">
             <label className="login label">Contraseña</label>
-            <input type="password" className="form-control" name="contrasenia"  />
+            <input 
+              type="password" 
+              className="form-control" 
+              name="contrasenia" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} 
+            />
           </div>
           <br />
           <button type="submit" className="login btn btn-primary">
             Iniciar Sesión
           </button>
           <label className="login label">Registrarse</label>
+          {alert && <div className="alert alert-danger">{alert}</div>}
         </form>
       </div>
     </div>
